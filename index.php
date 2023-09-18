6d2013e55f1df3a08981c93b1aa0ec4a20818ceb
cruscaya <ronaldo12roni@gmail.com>
2023-09-17 23:41:46 -0500

    DñoMenuPrincipal

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 session_start();
 if (isset($_SESSION['email'])) {
-  header('Location: allTasks.php');
+  header('Location: MenuPrincipal.php');
   exit();
 }
 
---
+++ userNavbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar">
+  <h1 class="nav-brand">Aplicación ToDoList</h1>
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="">Perfil</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link logout" href="">Cerrar sesión</a>
+    </li>
+  </ul>
+</nav>
